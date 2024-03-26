6fd38eeb21d1ee351ad2eb8380934618edca17a2
lepakshiadni <lepakshichowdary006@gmail.com>
2024-03-26 10:02:51 +0530

    git commit

---
+++ route/trainercreatepostroute.js
@@ -0,0 +1,25 @@
+const route = require('express').Router()
+
+const {
+    trainerCreatePost, addTrainerPostComments, addLikeToTrainerPost,getTrainerPostBy,
+    getTrainierPostComments, getpostTrainerPost, getpostTrainercreatePostById, deleteTrainerPostComment, hidePost
+
+} = require('../controllers/trainercreatepostctrl')
+const { jwtverify } = require('../middleware/jwtverify')
+
+const multer = require('multer')
+
+const upload = multer({ storage: multer.memoryStorage() })
+
+route.post('/trainerCreatePost', jwtverify, upload.single('postImg'), trainerCreatePost)
+route.put('/addTrainerPostComments/:postId', addTrainerPostComments)
+route.delete('/deleteTrainerPostComment/:postId/:commentId', deleteTrainerPostComment)
+route.put('/addLikeToTrainerPost/:postId', addLikeToTrainerPost)
+route.get('/getTrainierPostComments/:postId', getTrainierPostComments)
+route.get('/getpostTrainingRequirement', getpostTrainerPost)
+route.get('/getpostTrainercreatePostById/:postId', getpostTrainercreatePostById)
+route.get('/getTrainerPostBy',jwtverify, getTrainerPostBy)
+route.post('/hidePost/:postId', hidePost)
+
+
+module.exports = route;
